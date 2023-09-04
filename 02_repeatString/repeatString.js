9c39b6906999f8e1eba11812d8c86ee9b7e69960
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-04 11:36:31 -0500

    Add solved lessons 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let entireString ="";
+    if (num >= 0) {
+        for (i = 1; i <= num; i++) {
+            entireString = entireString + string;
+            
+        }
+        return entireString;
+    } else {
+        return "ERROR";
+    }
+   
 };
 
 // Do not edit below this line
