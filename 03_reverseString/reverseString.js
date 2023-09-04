ab3441e237c5af8bd3afdd2318f54be12a7b01d6
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-04 16:30:36 -0500

    Add solved lesson 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    //console.log(string.length);
+    for (i = string.length; i >= 0; i--) {
+       reversedString = reversedString + string.charAt(i);
+    }
+    return reversedString;
 };
+
+reverseString('olleH');
 
 // Do not edit below this line
 module.exports = reverseString;
