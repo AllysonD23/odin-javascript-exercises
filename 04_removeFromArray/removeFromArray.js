a3ca18fc81ba6dc58e95c47c09f177fdb3084ff5
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-07 19:36:05 -0500

    Add solved lesson 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+// Create a function that removes unlimited specified items (argument 2) in an array (argument 1).
 
-};
+    const removeFromArray = function(array, ...args) {
+        let result = array;
+        for (let i = 0; i <= args.length; i++) {
+        result = result.filter((item) => item !== args[i]); 
+        }
+            return result;
+    };
+    
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
