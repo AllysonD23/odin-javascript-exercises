729ecdfe0adbcd1f191872afa3ff8c8e2f21074a
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-21 16:54:27 -0500

    Add unsolved lesson 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let punctuationLess = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    let array = punctuationLess.split('');
+    console.log(array);
+    let reversedArray = array.reverse();
+    console.log(reversedArray);
+    // for (i = 0; i <= array.length; i++) {
+        let palindromesCheck = (array) => array == reversedArray;
+        console.log(array.every(palindromesCheck));
+        // if (array[i] != reversedArray[i]) {
+        //     return false;
+        // } else return true;
+// }
 };
 
 // Do not edit below this line
