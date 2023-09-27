2b799b3d64e1be874ece60c7f728f42e7182c697
AllysonD23 <117053193+AllysonD23@users.noreply.github.com>
2023-09-26 20:41:04 -0500

    Add solved lesson 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(books) {
+    let titleArray = [];
+    
+    for (let i = 0; i < books.length; i++) {
+        titleArray.push(books[i].title);
+    }
+    
+    return titleArray;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
